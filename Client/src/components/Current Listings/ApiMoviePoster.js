6266fcd107c17514c7c1b4ef4f24a91f232cc304
Kiregor <kiregorekal@gmail.com>
2019-06-20 00:07:20 +0100

    first commit

---
+++ Client/src/components/Current Listings/ApiMoviePoster.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Movie from './ApiMovie2';
+
+const MoviePoster = (props) => {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col s12">
+                    {
+                        props.movies.map((movie, i, ageRating) => {
+                            return (
+                                <Movie key={i} image={movie.poster_path} title={movie.title} overview={movie.overview} id={movie.id} ageRating={ageRating} />
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MoviePoster;
